0527d3fc7a0cbb12fdb57ac7853a5ec523c2d1e4
sgg <376866962@qq.com>
2020-06-17 21:19:50 +0800

    索旭廷6.17作业

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -1,7 +1,18 @@
 package day03;
 
 public class HomeWork {
-        //打印99乘法表
 
         //打印等腰三角形
+        public static void main(String[] args) {
+            int num = 6;
+            for (int i = 0; i < num; i++) {
+                for (int j = 0; j < num-i-1; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k <(i*2-1) ; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
 }
